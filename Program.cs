24a88d023e553a7bdb99ed3534080508570d47fb
Ana73l <you@example.com>
2017-02-17 16:17:34 +0200

    changed GetHtml to return string

--- Program.cs
+++ Program.cs
@@ -10,7 +10,7 @@ namespace BananasRouter
 {
     class Program
     {
-        static void GetHtml(string content, int pageNum)
+        static string GetHtml(string content, int pageNum)
         {
             string sUrl = "http://zamunda.net/bananas?c42=1&c25=1&c35=1&c46=1&c20=1&c19=1&c5=1&c24=1&c31=1&c28=1&c7=1&c33=1&c39=1&c4=1&c21=1&c17=1&c40=1&c12=1&c6=1&c30=1&c29=1&c51=1&c34=1&c38=1&c1=1&c22=1&c43=1&c41=1&c36=1&c52=1&c53=1&c26=1&c23=1&c32=1&c37=1&search=" + string.Join("+", content.Split().ToArray()) + "&page=" + (pageNum - 1);
             WebRequest request = WebRequest.Create(sUrl);
@@ -20,8 +20,8 @@ namespace BananasRouter
             sr.Close();
             response.Close();
 
-            Console.WriteLine(result);
             System.IO.File.WriteAllText("text.txt", result);
+            return result;
         }
 
         static void Main(string[] args)
@@ -29,7 +29,7 @@ namespace BananasRouter
             string content = Console.ReadLine();
             int index = int.Parse(Console.ReadLine());
 
-            GetHtml(content, index);
+            Console.WriteLine(GetHtml(content, index));
 
             Console.ReadKey();
         }
